5e169be2599dfe3ee3275b3b4d790132dea9e224
msilvaj <miqueias.ufpi@gmail.com>
2020-03-17 14:49:47 -0300

    Add gem pg and config to create Postgres databases

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2020_03_17_171136) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "usuarios", force: :cascade do |t|
     t.boolean "admin"
     t.string "nome"
