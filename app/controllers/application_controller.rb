28eaff5e0852e2ec8ed8210cff69b3609ca470f4
msilvaj <miqueias.ufpi@gmail.com>
2020-03-19 10:14:10 -0300

    Configure alias to method current_user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,15 +5,21 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_usuario!
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-    protected
-
-      def configure_permitted_parameters
-        devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :nome, :peso, :pesoIdeal, :altura, :location])
-        
-      end
-
   def current_user
     @current_user ||= session[:current_usuario_id] && Usuario.find_by_id(session[:current_usuario_id])
   end
-  helper_method :current_user
+
+  alias_method :current_user, :current_usuario 
+
+  def current_ability
+    @current_ability ||= Ability.new(current_user)
+  end
+  helper_method :current_ability
+
+  protected
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up, keys: [:email, :nome, :peso, :pesoIdeal, :altura, :location])
+
+    end
 end
